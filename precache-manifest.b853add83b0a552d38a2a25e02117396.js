67f1e71d42e4b124903f4ee9f03f413697bfd326
Jessica <jessikurt@gmail.com>
2020-08-18 11:57:52 +0100

    Updates

---
+++ precache-manifest.b853add83b0a552d38a2a25e02117396.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076d87b48d0314f720874a78682a4705",
+    "url": "/repositories-course/index.html"
+  },
+  {
+    "revision": "6d7facca3e7eff438e4d",
+    "url": "/repositories-course/static/js/2.b4cc088c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/repositories-course/static/js/2.b4cc088c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4132af0e552658b409a6",
+    "url": "/repositories-course/static/js/main.c203b903.chunk.js"
+  },
+  {
+    "revision": "9fe3c74bab595e0f419c",
+    "url": "/repositories-course/static/js/runtime-main.9ade9d26.js"
+  }
+]);
